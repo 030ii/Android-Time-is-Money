789a4a4e53fb7429fd900db5c38354cff15fdf75
030ii <lovelygeee2@gmail.com>
2017-06-02 14:57:14 +0900

    tab layout

---
+++ TIM/app/src/main/java/com/example/helllo/tim/Fragment3.java
@@ -0,0 +1,20 @@
+package com.example.helllo.tim;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Fragment3 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment3, container, false);
+
+        return rootView;
+    }
+}
---
+++ TIM/app/src/main/java/com/example/helllo/tim/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.helllo.tim;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+    Fragment1 fragment1;
+    Fragment2 fragment2;
+    Fragment3 fragment3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        fragment1 = new Fragment1();
+        fragment2 = new Fragment2();
+        fragment3 = new Fragment3();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.container, fragment1).commit();
+
+        TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+        tabs.addTab(tabs.newTab().setText("오늘 기록"));
+        tabs.addTab(tabs.newTab().setText("전체 기록"));
+        tabs.addTab(tabs.newTab().setText("업적"));
+
+        tabs.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+
+                Fragment selected = null;
+                if (position == 0) {
+                    selected = fragment1;
+                } else if (position == 1) {
+                    selected = fragment2;
+                } else if (position == 2) {
+                    selected = fragment3;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, selected).commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                // 다른 탭 눌렀을 때
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                // 현재 탭 눌렀을 때
+            }
+        });
+    }
+}
